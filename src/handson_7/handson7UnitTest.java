ad907481dd632e7e6af61719b92b4bfe7f54b597
sudhanshucena <sudhanshucena@gmail.com>
2015-10-14 08:00:28 +0100

    task Completed

--- src/handson_7/handson7UnitTest.java
+++ src/handson_7/handson7UnitTest.java
@@ -1,14 +1,15 @@
 package handson_7;
-
+import handson_7.Sam;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class handson7UnitTest {
-
+	Sam object= new Sam(10);
+	Sam fobject = new Sam(10.2);
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertTrue(object.ivalue instanceof Integer);
+		assertTrue(fobject.fvalue instanceof Double);
 	}
-
 }
--- src/handson_7/sam.java
+++ src/handson_7/sam.java
@@ -1,5 +1,15 @@
 package handson_7;
 
-public class sam {
-
+public class Sam {
+	Integer ivalue;
+	Double fvalue;
+	Sam(){
+		//do nothing
+	}
+	Sam(Integer intvalue){
+		ivalue = intvalue;
+	}
+	Sam(Double floatValue){
+		fvalue = floatValue;
+	}
 }
